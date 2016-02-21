94f95658bac307c67e46a9b9a3e5dd43e3507757
Baus <eltonsia@yahoo.com>
2016-02-21 02:14:01 -0500

    Completed sign up page. Started the login page and the forgot password page

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
 
+#Mongodb Settings
 app.config['MONGO_DBNAME'] = 'gdohs'
 
+
+#Enable extensions for flask
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 
@@ -34,38 +37,57 @@ def signUp():
 	_firstname = request.form['firstname']
 	_lastname = request.form['lastname']
 
-
-	#Check if any of ^ have any spaces before going below
-
-	string = "test this 	thing	\n"
-	print re.sub('[\s+]', '', string)
-
-	#If username already exists in database, send to sign up failure page
-	if (mongo.db.user.find_one({"username": _username})):
-		return render_template("signupfail.html", code=307)
+	#If all user inputs have no whitespaces, proceed with sign up
+	if (checkWhitespace(_username) and checkWhitespace(_password) and checkWhitespace(_email) and
+		checkWhitespace(_firstname) and checkWhitespace(_lastname)):
+		#If username already exists in database, send to sign up failure page
+		if (mongo.db.user.find_one({"username": _username})):
+			return render_template("signupfail.html", code=307)
+		else:
+			#Encrypt the password
+			pw_hash = bcrypt.generate_password_hash(_password)
+			#Add the new user into the database
+			result = mongo.db.user.insert_one(
+				{
+					"username": _username,
+					"password": pw_hash,
+					"email": _email,
+					"firstname": _firstname,
+					"lastname": _lastname
+				}
+			)
+		#Send to sign up success page if user was able to complete the sign up
+		return render_template("signupsuccess.html", code=307)
 	else:
-		#Encrypt the password
-		pw_hash = bcrypt.generate_password_hash(_password)
-		#Add the new user into the database
-		result = mongo.db.user.insert_one(
-			{
-				"username": _username,
-				"password": pw_hash,
-				"email": _email,
-				"firstname": _firstname,
-				"lastname": _lastname
-			}
-		)
-	#Send to sign up success page if user was able to complete the sign up
-	return render_template("signupsuccess.html", code=307)
+		return render_template("index.html")
+
+@app.route("/showForgotPassword")
+def showForgotPassword():
+	#Redirect to the forgot password page
+	return render_template("forgotpassword.html")
 
 @app.route("/checkUser", methods=['POST'])
 def checkUser():
 	return
 
-@app.route("/signUpSuccessful")
-def signUpSuccessful():
-	return
+##########################################################################
+#
+#							HELPER FUNCTIONS
+#
+##########################################################################
+
+def checkWhitespace(word):
+	#Checks for all types of whitespaces including \t, \n, \f, \v
+	ws = re.search('[\s+]', word)
+	#True for no whitespace
+	checker = True
+
+	#If there is whitespace then set checker to false
+	if (ws):
+		checker = False
+
+	return checker
+
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
