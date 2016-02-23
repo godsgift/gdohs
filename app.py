1e754995fc0bb944c4618bdf2f86cebefb444e1a
godsgift <eltonsia@yahoo.com>
2016-02-22 16:06:40 -0800

    Compelted new sign up, started password reset

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import re
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import *
 from flask.ext.pymongo import PyMongo
 # from flask.ext.security import Security
@@ -14,7 +15,7 @@ app.config['MONGO_DBNAME'] = 'gdohs'
 #need to add username and pass for db
 
 #WTFORMS Settings
-app.secret_key= 'testing'
+app.secret_key = 'testing'
 
 #Enable extensions for flask
 mongo = PyMongo(app)
@@ -23,11 +24,17 @@ bcrypt = Bcrypt(app)
 #Running db:
 #mongod --dbpath data
 
+##########################################################################
+#
+#								LOGGED OUT
+#
+##########################################################################
 
 @app.route("/")
 def index():
+	form = Login()
 	#Redirect to home page
-	return render_template("index.html")
+	return render_template("index.html", form=form)
 
 @app.route("/showSignup")
 def showSignup():
@@ -38,65 +45,80 @@ def showSignup():
 @app.route("/signUp", methods=['GET', 'POST'])
 def signUp():
 	if (request.method == 'POST'):
+		#Grab the data from user input
 		form = SignUp(request.form)
-
+		#If the form has been validated
 		if form.validate_on_submit():
+			#Grab data from each field
 			_username = form.username.data
 			_password = form.password.data
 			_email = form.email.data
 			_firstname = form.firstname.data
 			_lastname = form.lastname.data
 
-			print _username + _password + _email + _firstname + _lastname
-			#check if username exists in database
-			
-			print "Username VALIDATED"
+			#If all user inputs have no whitespaces, proceed with sign up
+			if (checkWhitespace(_username) and checkWhitespace(_password) and checkWhitespace(_email) and
+				checkWhitespace(_firstname) and checkWhitespace(_lastname)):
+				#If username already exists, send an error to the page
+				if (mongo.db.user.find_one({"username": _username})):
+					form.username.errors.append("Username already taken")
+					return render_template('signup.html', form=form)
+				elif (mongo.db.user.find_one({"email": _email})):
+					form.email.errors.append("Email already taken")
+				else:
+					#Encrypt the password
+					pw_hash = bcrypt.generate_password_hash(_password)
+					#Add the new user into the database
+					result = mongo.db.user.insert_one(
+						{
+							"username": _username,
+							"password": pw_hash,
+							"email": _email,
+							"firstname": _firstname,
+							"lastname": _lastname
+						}
+					)
+				#Send to sign up success page if user was able to complete the sign up
+				return render_template("signupsuccess.html")
+			else:
+				return render_template('signup.html', form=form)
 		else:
-			print"USERNAME FAILED"
 			return render_template('signup.html', form=form)
 	return render_template('signup.html', form=SignUp())
 
-# @app.route("/signUp", methods=['POST', 'GET'])
-# def signUp():
-# 	#Grab user input
-# 	_username = request.form['username']
-# 	_password = request.form['password']
-# 	_email = request.form['email']
-# 	_firstname = request.form['firstname']
-# 	_lastname = request.form['lastname']
-
-# 	#If all user inputs have no whitespaces, proceed with sign up
-# 	if (checkWhitespace(_username) and checkWhitespace(_password) and checkWhitespace(_email) and
-# 		checkWhitespace(_firstname) and checkWhitespace(_lastname)):
-# 		#If username already exists in database, send to sign up failure page
-# 		if (mongo.db.user.find_one({"username": _username})):
-# 			return render_template("signupfail.html", code=307)
-# 		else:
-# 			#Encrypt the password
-# 			pw_hash = bcrypt.generate_password_hash(_password)
-# 			#Add the new user into the database
-# 			result = mongo.db.user.insert_one(
-# 				{
-# 					"username": _username,
-# 					"password": pw_hash,
-# 					"email": _email,
-# 					"firstname": _firstname,
-# 					"lastname": _lastname
-# 				}
-# 			)
-# 		#Send to sign up success page if user was able to complete the sign up
-# 		return render_template("signupsuccess.html", code=307)
-# 	else:
-# 		return render_template("index.html")
-
 @app.route("/showForgotPassword")
 def showForgotPassword():
+	form = ForgotPassword()
 	#Redirect to the forgot password page
-	return render_template("forgotpassword.html")
+	return render_template("forgotpassword.html", form=form)
 
-@app.route("/checkUser", methods=['POST'])
-def checkUser():
+@app.route("/forgotPassword", methods=['GET', 'POST'])
+def forgotPassword():
+
+	if (request.method == 'POST'):
+		#create token
+		token = request.args.get('token', None)
+		form = ForgotPassword(request.form)
+
+		if form.validate_on_submit():
+			#get the email
+			_email = form.email.data
+			checkEmail = mongo.db.user.find_one({"email": _email})
+			if (checkEmail):
+				token = checkEmail.get_token()
+				print token
+		return render_template("/forgotpassword.html", form=form)
+
+
+@app.route("/resetPassword")
+def resetPassword():
+	form = NewPassword()
+	return render_template("newpassword.html", form=form)
+
+@app.route("/login")
+def login():
 	return
+
 
 ##########################################################################
 #
@@ -116,11 +138,26 @@ class SignUp(Form):
 		Regexp(r'^[\w.@+-]+$', message="Please provide an email without any spaces")])
 
 	firstname = TextField("First Name", validators=[Required("Please provide your first name"),
-		Regexp(r'^[\w.@+-]+$')])
+		Regexp(r'^[\w.@+-]+$', message="Please enter your first name without any spaces")])
 
 	lastname = TextField("Last Name", validators=[Required("Please provide your last name"),
-		Regexp(r'^[\w.@+-]+$')])
+		Regexp(r'^[\w.@+-]+$', message="Please enter your last name without any spaces")])
 
+class Login(Form):
+	username = TextField("Username", validators=[Required("Please provide a username without any spaces"),
+		Length(min=4, max=20), Regexp(r'^[\w.@+-]+$', message="Please provide a username without any spaces")])
+
+	password = PasswordField("Password", validators=[Required("Please pick a secure password"),
+		Regexp(r'^[\w.@+-]+$', message="Please provide a password without any spaces")])
+
+class ForgotPassword(Form):
+	email = TextField("Email", validators=[Required("Please provide a valid email address"),
+		Length(min=6, max=35), Email(message="That is not a valid email address"),
+		Regexp(r'^[\w.@+-]+$', message="Please provide an email without any spaces")])
+
+class NewPassword(Form):
+	password = PasswordField("Password", validators=[Required("Please pick a secure password"),
+		Regexp(r'^[\w.@+-]+$', message="Please provide a password without any spaces")])
 
 ##########################################################################
 #
@@ -139,6 +176,10 @@ def checkWhitespace(word):
 		checker = False
 
 	return checker
+
+def get_token(self, expiration=100):
+	s = Serializer(app.secret_key, expiration)
+	return s.dumps({'username': self.id}).decode('utf-8')
 
 
 if __name__ == "__main__":
---
+++ tests/test.py
@@ -0,0 +1,24 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, URLSafeSerializer
+from pymongo import MongoClient
+import json
+
+client = MongoClient()
+# client = MongoClient("mongodb://mongodb0.example.net:27019")
+
+#db = client.name of database
+db = client.gdohs
+
+
+test = db.user.find_one({"username": "test"})
+
+
+print test
+
+
+
+# user = "test"
+
+# s = URLSafeSerializer("super-secret-key")
+# link = s.dumps(user)
+# print link
+# print s.loads(link)
